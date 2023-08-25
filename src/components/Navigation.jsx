fd21ae2291096d14ebf95f56c8cdf443068b0e64
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-08-25 14:31:23 +0200

    Add videos to header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,7 @@ const navigation = [
   { name: 'Hjem', href: '/' },
   { name: 'Repertoar', href: '/repertoar' },
   { name: 'Artister', href: '/#artister' },
+  { name: 'Videoer', href: '/#videoer' },
   { name: 'Kontakt', href: '/kontakt' },
 ];
 
