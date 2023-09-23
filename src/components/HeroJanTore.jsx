ef92f423640cf0c744a9c133a9874a2d2e6bda91
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-09-23 09:08:10 +0200

    add jan-tore

--- src/components/HeroJanTore.jsx
+++ src/components/HeroJanTore.jsx
@@ -5,20 +5,21 @@ import { useState } from 'react';
 import { Dialog } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
-export function HeroNilsGeorg() {
+export function HeroJanTore() {
   return (
     <div className="bg-gray-900 ">
       <div className="relative grid grid-cols-1 overflow-hidden isolate pt-14 sm:grid-cols-2">
         <Image
-          src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Sang%20i%20begravelse/NG_profile_mobile_kbfder.webp"
-          alt="Nils Georg Nilsen"
+          src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Sang%20i%20begravelse/artists/JT_mobile_zy5y5u.webp"
+          alt="Jan-Tore Saltnes"
           width={2073}
           height={1887}
           className="absolute inset-0 object-cover w-full h-full sm:hidden -z-10"
         />
+        <div className="absolute inset-0 bg-gray-950/20" />
         <Image
-          src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Sang%20i%20begravelse/NG_profile_ho4l6x.webp"
-          alt="Nils Georg Nilsen"
+          src="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Sang%20i%20begravelse/artists/JT_desktop_gyn4mv.webp"
+          alt="Jan-Tore Saltnes"
           width={2830}
           height={1887}
           className="absolute inset-0 hidden object-cover w-full h-full sm:block -z-10"
@@ -27,10 +28,10 @@ export function HeroNilsGeorg() {
           className="absolute inset-x-0 overflow-hidden -top-40 -z-10 transform-gpu blur-3xl sm:-top-80"
           aria-hidden="true"
         ></div>
-        <div className="max-w-md py-32 pr-6 mx-auto sm:mr-12 sm:-ml-12 sm:col-start-2 sm:max-w-2xl md:max-w-3xl sm:py-48 lg:py-56 sm:mx-0">
+        <div className="relative z-10 max-w-md py-32 pr-6 mx-auto sm:mr-12 sm:-ml-12 sm:col-start-2 sm:max-w-2xl md:max-w-3xl sm:py-48 lg:py-56 sm:mx-0">
           <div className="text-center sm:text-left">
             <h6 className="pt-20 text-5xl tracking-tight text-white sm:text-slate-700 sm:text-6xl lg:text-7xl">
-              Nils Georg Nilsen
+              Jan-Tore Saltnes
             </h6>
 
             <motion.p
@@ -43,8 +44,8 @@ export function HeroNilsGeorg() {
               }}
               className="hidden px-10 mx-auto mt-6 leading-8 text-gray-600 max-w-[100px] sm:px-0 sm:max-w-lg sm:text-lg lg:max-w-xl sm:mx-0 sm:block "
             >
-              Allsidig sanger med imponerende prestasjoner og et omfattende
-              operarepertoar.
+              En etterspurt sanger for seremonier med variert musikkstil i
+              Vestfold.
             </motion.p>
 
             <div className="flex items-center justify-center mt-10 sm:justify-start gap-x-6">
@@ -68,15 +69,7 @@ export function HeroNilsGeorg() {
         <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
-        >
-          {/* <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          /> */}
-        </div>
+        ></div>
       </div>
     </div>
   );
---
+++ src/pages/jan-tore-saltnes.jsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+
+import { HeroJanTore } from '@/components/HeroJanTore';
+import { JanTore } from '@/components/JanTore';
+
+export default function JanToreSaltnes() {
+  return (
+    <>
+      <Head>
+        <title>
+          Jan-Tore Saltnes – Profesjonell sanger for begravelser i Vestfold
+        </title>
+        <meta
+          name="description"
+          content="Profesjonell sanger for begravelser i Vestfold med variert repertoar og dyp musikalsk erfaring."
+        />
+        <link
+          rel="canonical"
+          href="https://sangibegravelse.no/jan-tore-saltnes"
+          key="canonical"
+        />
+      </Head>
+      <div className="bg-white">
+        <main>
+          <HeroJanTore />
+          <JanTore />
+        </main>
+      </div>
+    </>
+  );
+}
