4201e472f5f002c0742f83a16e88a3c9d9f85ae1
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-10-19 22:27:03 +0200

    break repertoar toggle into components

---
+++ src/components/RepertoireList.jsx
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline';
+import { Expandable } from '@/components/Expandable';
+import { TableHeader } from './TableHeader';
+import { SongDetails } from './SongDetails';
+import { ViolinIcon } from './icons/Violin';
+import { SoloIcon } from './icons/Solo';
+import { DuetIcon } from './icons/Duet';
+
+export function RepertoireList() {
+  const [repertoire, setRepertoire] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedSubcategory, setSelectedSubcategory] = useState('');
+  const [selectedArrangement, setSelectedArrangement] = useState('');
+
+  useEffect(() => {
+    fetch('/api/repertoire')
+      .then((response) => response.json())
+      .then((data) => {
+        const sortedRepertoire = data.sort((a, b) => {
+          // Custom sorting function considering the Norwegian alphabet
+          const collator = new Intl.Collator('nb');
+          return collator.compare(a.title, b.title);
+        });
+        setRepertoire(sortedRepertoire);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  // Filter function to apply selected filters
+  const filterSongs = (song) => {
+    const filters = [
+      [selectedCategory, song.category],
+      [selectedSubcategory, song.subcategory],
+      [selectedArrangement, song.arrangement],
+    ];
+
+    return filters.every(([selected, property]) => {
+      return !selected || property.includes(selected);
+    });
+  };
+
+  // Event handlers for category and arrangement selection
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleSubcategoryChange = (event) => {
+    setSelectedSubcategory(event.target.value);
+  };
+
+  const handleArrangementChange = (event) => {
+    setSelectedArrangement(event.target.value);
+  };
+
+  const [expandedSongs, setExpandedSongs] = useState([]);
+
+  const toggleSong = (songId) => {
+    setExpandedSongs((prevExpandedSongs) => {
+      if (prevExpandedSongs.includes(songId)) {
+        return prevExpandedSongs.filter((id) => id !== songId);
+      } else {
+        return [...prevExpandedSongs, songId];
+      }
+    });
+  };
+
+  const categoryStyles = {
+    Salmer: 'text-lime-700 bg-lime-50 ring-lime-600/20',
+    Pop: 'text-yellow-700 bg-yellow-50 ring-yellow-600/20',
+    Viser: 'text-blue-700 bg-blue-50 ring-blue-600/20',
+    Klassisk: 'text-rose-700 bg-rose-50 ring-rose-600/20',
+    Musikal: 'text-yellow-700 bg-yellow-50 ring-yellow-600/20',
+    'Rock/Soul/R&B': 'text-purple-700 bg-purple-50 ring-purple-600/20',
+    Folkemusikk: 'text-indigo-700 bg-indigo-50 ring-indigo-600/20',
+    Jazz: 'text-sky-700 bg-sky-50 ring-sky-600/20',
+  };
+
+  // Utility function to get the arrangement icon
+  const getArrangementIcon = (arrangement, type, IconComponent) => {
+    return arrangement.includes(type) ? (
+      <span className="inline-flex items-center p-1.5 text-xs font-medium text-slate-700 rounded-full bg-slate-50 ring-1 ring-inset ring-slate-600/20">
+        <IconComponent className="w-3 h-3 fill-slate-600" aria-hidden="true" />
+      </span>
+    ) : null;
+  };
+
+  return (
+    <div className="mt-8 -mx-4 sm:-mx-0">
+      <table className="min-w-full divide-y divide-gray-300">
+        <TableHeader />
+        <tbody className="bg-white divide-y divide-gray-200">
+          {repertoire.filter(filterSongs).map((sang) => (
+            <React.Fragment key={sang.objectID}>
+              <tr>
+                <td className="w-full py-4 pl-4 pr-3 text-sm font-medium text-gray-900 max-w-0 sm:w-auto sm:max-w-none sm:pl-0">
+                  {sang.title}
+                  <dl className="font-normal lg:hidden">
+                    <dt className="sr-only">Composer</dt>
+                    <dd className="mt-1 text-gray-700 truncate">
+                      {sang.composer}
+                    </dd>
+                  </dl>
+                </td>
+                <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                  {sang.composer}
+                </td>
+                <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                  <div className="flex space-x-1">
+                    {getArrangementIcon(sang.arrangement, 'solo', SoloIcon)}
+                    {getArrangementIcon(sang.arrangement, 'duett', DuetIcon)}
+                    {getArrangementIcon(sang.arrangement, 'fiolin', ViolinIcon)}
+                  </div>
+                </td>
+                <td className="px-3 py-4 text-sm text-gray-500">
+                  {sang.category && (
+                    <span
+                      className={`inline-flex items-center px-2 py-1 text-xs font-medium rounded-md ring-1 ring-inset ring-600/20 ${
+                        categoryStyles[sang.category]
+                      }`}
+                    >
+                      {sang.category}
+                    </span>
+                  )}
+                </td>
+                <td className="py-4 pl-3 pr-4 text-sm font-medium text-right sm:pr-0">
+                  <button
+                    className="ml-2 focus:outline-none"
+                    onClick={() => toggleSong(sang.objectID)}
+                  >
+                    {expandedSongs.includes(sang.objectID) ? (
+                      <MinusSmallIcon className="w-6 h-6" aria-hidden="true" />
+                    ) : (
+                      <PlusSmallIcon className="w-6 h-6" aria-hidden="true" />
+                    )}
+                  </button>
+                </td>
+              </tr>
+              <Expandable>
+                {({ isExpanded }) => (
+                  <SongDetails
+                    isExpanded={isExpanded}
+                    expandedSongs={expandedSongs}
+                    sang={sang}
+                  />
+                )}
+              </Expandable>
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
---
+++ src/components/SongDetails.jsx
@@ -0,0 +1,180 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { Disclosure } from '@headlessui/react';
+import { MinusSmallIcon, PlusSmallIcon } from '@heroicons/react/24/outline';
+import { Expandable } from '@/components/Expandable';
+import { ViolinIcon } from './icons/Violin';
+import { SoloIcon } from './icons/Solo';
+import { DuetIcon } from './icons/Duet';
+
+export function SongDetails({ isExpanded, expandedSongs, sang }) {
+  const [repertoire, setRepertoire] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [selectedSubcategory, setSelectedSubcategory] = useState('');
+  const [selectedArrangement, setSelectedArrangement] = useState('');
+
+  useEffect(() => {
+    fetch('/api/repertoire')
+      .then((response) => response.json())
+      .then((data) => {
+        const sortedRepertoire = data.sort((a, b) => {
+          // Custom sorting function considering the Norwegian alphabet
+          const collator = new Intl.Collator('nb');
+          return collator.compare(a.title, b.title);
+        });
+        setRepertoire(sortedRepertoire);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  // Filter function to apply selected filters
+  const filterSongs = (song) => {
+    if (selectedCategory && song.category !== selectedCategory) {
+      return false;
+    }
+    if (
+      selectedSubcategory &&
+      !song.subcategory.includes(selectedSubcategory)
+    ) {
+      return false;
+    }
+    if (
+      selectedArrangement &&
+      !song.arrangement.includes(selectedArrangement)
+    ) {
+      return false;
+    }
+    return true;
+  };
+
+  // Event handlers for category and arrangement selection
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleSubcategoryChange = (event) => {
+    setSelectedSubcategory(event.target.value);
+  };
+
+  const handleArrangementChange = (event) => {
+    setSelectedArrangement(event.target.value);
+  };
+
+  // const [expandedSongs, setExpandedSongs] = useState([]);
+
+  const toggleSong = (songId) => {
+    setExpandedSongs((prevExpandedSongs) => {
+      if (prevExpandedSongs.includes(songId)) {
+        return prevExpandedSongs.filter((id) => id !== songId);
+      } else {
+        return [...prevExpandedSongs, songId];
+      }
+    });
+  };
+
+  return (
+    <>
+      {expandedSongs.includes(sang.objectID) && (
+        <tr>
+          <td colSpan="5">
+            <div
+              id="expanded"
+              className="grid w-full grid-cols-1 gap-4 p-4 md:grid-cols-2"
+            >
+              <div className="p-3">
+                <p className="max-w-lg text-sm leading-7 text-gray-600 ">
+                  {sang.description.map((line) => (
+                    <div className="mt-3" key={line}>
+                      <p>{line}</p>
+                    </div>
+                  ))}
+                </p>
+                <div className="text-gray-700">
+                  {sang.lyrics && (
+                    <span>
+                      <Expandable.Button>
+                        <p className="">Tekst</p>
+                      </Expandable.Button>
+                    </span>
+                  )}
+                </div>
+                {isExpanded ? (
+                  <>
+                    {sang.lyrics && (
+                      <div className="p-8 mt-6 mb-2 overflow-hidden rounded-lg bg-amber-50/70 w-fit">
+                        <p className="max-w-lg text-sm font-medium leading-7 text-gray-600 ">
+                          {sang.lyrics.map((line, i) => (
+                            <div className="mt-0" key={i}>
+                              {line === '' ? <br /> : <p>{line}</p>}
+                            </div>
+                          ))}
+                        </p>
+                      </div>
+                    )}
+                  </>
+                ) : null}
+
+                <div className="pt-6">
+                  <p className="text-sm font-semibold text-gray-900">Språk</p>
+                  <p className="flex max-w-md gap-3 text-sm leading-7 text-gray-600">
+                    {sang.subcategory.map((line) => (
+                      <div className="mt-3" key={line}>
+                        <span className="inline-flex items-center px-2 py-1 text-xs font-medium rounded-md text-slate-700 bg-slate-50 ring-1 ring-inset ring-slate-600/20">
+                          {line}
+                        </span>
+                      </div>
+                    ))}
+                  </p>
+                  <>
+                    {sang.audio && (
+                      <div className="mt-6">
+                        <audio controls>
+                          <source src={sang.audioUrl} type="audio/mpeg" />
+                          Your browser does not support the audio element.
+                        </audio>
+                      </div>
+                    )}
+                    {sang.performers && (
+                      <p className="max-w-lg pt-4 text-sm leading-7 text-gray-600">
+                        {sang.performers}
+                      </p>
+                    )}
+                  </>
+                </div>
+              </div>
+              <div
+                aria-hidden="true"
+                className="relative mt-2 mb-10 overflow-hidden rounded-lg sm:hidden"
+              >
+                <Image
+                  src={sang.mobile}
+                  alt={sang.title}
+                  width={640}
+                  height={359}
+                  className="object-cover object-center w-full h-64"
+                  unoptimized
+                />
+              </div>
+              <div
+                aria-hidden="true"
+                className="relative hidden overflow-hidden rounded-lg sm:block"
+              >
+                <Image
+                  src={sang.desktop}
+                  alt={sang.title}
+                  width={1456}
+                  height={816}
+                  className="object-cover object-center w-full overflow-hidden rounded-lg h-96"
+                  unoptimized
+                />
+              </div>
+            </div>
+          </td>
+        </tr>
+      )}
+    </>
+  );
+}
