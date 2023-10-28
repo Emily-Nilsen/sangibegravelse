1193cd1c590792af4e264d526bf409dece748fb0
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-10-28 11:03:00 +0200

    edit schema and OG data on homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,17 +11,60 @@ export default function Home() {
     <>
       <Head>
         <title>
-          Sang i begravelse - Profesjonell begravelsessang og musikktjenester
+          Sang i begravelse - musikk fremført av profesjonelle artister
         </title>
         <meta
           name="description"
-          content="Finn de perfekte sangene for begravelser. Profesjonelle sangere og musikere med høy kvalitet er tilgjengelige. Vi hjelper pårørende med passende begravelsesmusikk."
+          content="Sang i begravelse tilbyr et stort sortiment av sanger egnet for bruk i begravelser. Utforsk vårt brede utvalg for å finne den perfekte sangen for deres minnestund. Vår nettside tilbyr all nødvendig informasjon for å gjøre et informert valg av sang til en verdig avskjed."
+        />
+        <meta
+          name="keywords"
+          content="begravelse, sang, musikk, Norge, artister, begravelsesmusikk, minneseremoni, urnenedsettelse, repertoar, repertoarliste, begravelsessang, begravelsessanger, sanger, fiolin, solo, duett, viser, salmer, pop, rock, soul, julesanger, klassisk, opera, folkemusikk, musikal, jazz, R&B"
         />
         <link
           rel="canonical"
           href="https://sangibegravelse.no/"
           key="canonical"
         />
+        {/* Open Graph Details */}
+        <meta
+          property="og:title"
+          content="Sang i begravelse - musikk fremført av profesjonelle artister"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://sangibegravelse.no/" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dt3k2apqd/image/upload/v1698149656/Sang%20i%20begravelse/Sang_i_begravelse_logo_l0ipsd.svg"
+        />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Sang%20i%20begravelse/Sang_i_begravelse_jfysbv.webp"
+        />
+        <meta
+          property="og:description"
+          content="Sang i begravelse tilbyr et stort sortiment av sanger egnet for bruk i begravelser. Utforsk vårt brede utvalg for å finne den perfekte sangen for deres minnestund. Vår nettside tilbyr all nødvendig informasjon for å gjøre et informert valg av sang til en verdig avskjed."
+        />
+        <meta property="og:locale" content="nb_NO" />
+        {/* Structured Data */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'WebSite',
+            url: 'https://sangibegravelse.no/',
+            name: 'Sang i begravelse',
+            logo: 'https://res.cloudinary.com/dt3k2apqd/image/upload/v1698149656/Sang%20i%20begravelse/Sang_i_begravelse_logo_l0ipsd.svg',
+            image:
+              'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Sang%20i%20begravelse/Sang_i_begravelse_jfysbv.webp',
+            potentialAction: {
+              '@type': 'SearchAction',
+              target: 'https://sangibegravelse.no/sok?q={search_term_string}',
+              'query-input': 'required name=search_term_string',
+            },
+            description:
+              'En plattform for å finne og velge sanger til begravelser eller minnestunder med mulighet til å engasjere profesjonelle norske artister.',
+          })}
+        </script>
       </Head>
       <div className="bg-white">
         <main>
