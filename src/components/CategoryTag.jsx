4abc7344670c06d86daa4757ef6e37ad81c133ff
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2023-11-02 19:31:59 +0100

    bring him home

--- src/components/CategoryTag.jsx
+++ src/components/CategoryTag.jsx
@@ -2,12 +2,12 @@ export function CategoryTag({ category, categoryVariable = 'category' }) {
   const categoryStyles = {
     Salmer: 'text-lime-700 bg-lime-50 ring-lime-600/20',
     Pop: 'text-yellow-700 bg-yellow-50 ring-yellow-600/20',
-    Viser: 'text-blue-700 bg-blue-50 ring-blue-600/20',
+    Viser: 'text-sky-700 bg-sky-50 ring-sky-600/20',
     Klassisk: 'text-rose-700 bg-rose-50 ring-rose-600/20',
-    Musikal: 'text-yellow-700 bg-yellow-50 ring-yellow-600/20',
+    Musikal: 'text-emerald-700 bg-emerald-50 ring-emerald-600/20',
     'Rock/Soul/R&B': 'text-purple-700 bg-purple-50 ring-purple-600/20',
     Folkemusikk: 'text-indigo-700 bg-indigo-50 ring-indigo-600/20',
-    Jazz: 'text-sky-700 bg-sky-50 ring-sky-600/20',
+    Jazz: 'text-fuchsia-700 bg-fuchsia-50 ring-fuchsia-600/20',
   };
 
   return (
