ec1fd005c8a5dc9b8ff053b4a2f3ed88b2eb36fb
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-01-06 17:56:59 +0100

    eternally

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://sangibegravelse.no/',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-};
--- src/pages/api/repertoire.js
+++
@@ -1,5 +0,0 @@
-import data from '../../lib/data.json';
-
-export default function handler(req, res) {
-  res.status(200).json(data);
-}
