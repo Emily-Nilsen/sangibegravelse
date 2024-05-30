10b576b7f46bbe0150faeceb8aab740e13e058bc
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-05-30 14:50:16 +0200

    nav menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 const navigation = [
   { name: 'Repertoar', href: '/' },
-  { name: 'Program Forslag', href: '/program-forslag' },
-  { name: 'Salmer og Fellessanger', href: '/salmer' },
+  { name: 'Programforslag', href: '/program-forslag' },
+  { name: 'Fellessanger', href: '/salmer' },
   { name: 'Om', href: '/om' },
   { name: 'Artister', href: '/artister' },
   { name: 'Kontakt', href: '/kontakt' },
