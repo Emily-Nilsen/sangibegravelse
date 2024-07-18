b10ba8ad18a9c78bbd56eb7ac2e098bc04972597
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-07-18 07:55:09 +0300

    add gtag to app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,11 @@
 import '@/styles/globals.css';
 import 'focus-visible';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
-import { BackToTopButton } from '@/components/BackToTopButton'; //
+import { BackToTopButton } from '@/components/BackToTopButton';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -30,6 +31,22 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-YSSCPN6MQ7"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YSSCPN6MQ7');
+          `,
+        }}
+      />
       <div className="relative bg-white">
         <Header textColour={textColour} />
         <main>
