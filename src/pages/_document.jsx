d69225effe9dec1097c7d03d16be12a8b20e4ac7
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2024-08-06 09:54:36 +0300

    kontakt hero images

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,10 +4,7 @@ export default function Document() {
   return (
     <Html lang="no">
       <Head>
-        <link
-          rel="icon"
-          href="https://res.cloudinary.com/dt3k2apqd/image/upload/v1698149656/Sang%20i%20begravelse/Sang_i_begravelse_logo_l0ipsd.svg"
-        />
+        <link rel="icon" href="/images/general/meta-logo.svg" />
         <link
           rel="stylesheet"
           href="https://use.typekit.net/sth1kfg.css"
--- src/pages/artister.jsx
+++ src/pages/artister.jsx
@@ -11,8 +11,7 @@ export default function Artister() {
     '@type': 'WebSite',
     url: 'https://sangibegravelse.no/artister',
     name: 'Sang i begravelse',
-    image:
-      'https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Sang%20i%20begravelse/Sang_i_begravelse_jfysbv.webp',
+    image: '/images/general/meta-img.webp',
     description:
       'En plattform for å finne og velge sanger til begravelser eller minnestunder med mulighet til å engasjere profesjonelle norske artister.',
     potentialAction: {
@@ -48,14 +47,8 @@ export default function Artister() {
         />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://sangibegravelse.no/artister" />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dt3k2apqd/image/upload/v1698149656/Sang%20i%20begravelse/Sang_i_begravelse_logo_l0ipsd.svg"
-        />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dt3k2apqd/image/upload/q_auto/Sang%20i%20begravelse/Sang_i_begravelse_jfysbv.webp"
-        />
+        <meta property="og:image" content="/images/general/meta-logo.svg" />
+        <meta property="og:image" content="/images/general/meta-img.webp" />
         <meta
           property="og:description"
           content="Sang i begravelse tilbyr et stort sortiment av sanger egnet for bruk i begravelser. Utforsk vårt brede utvalg for å finne den perfekte sangen for deres minnestund. Vår nettside tilbyr all nødvendig informasjon for å gjøre et informert valg av sang til en verdig avskjed."
