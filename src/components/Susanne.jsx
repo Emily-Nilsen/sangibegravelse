176f43a5502f514a3c48d416f129b1f9a7ec1132
EmilyJaneNilsen <emily.jane.nilsen@gmail.com>
2026-01-21 16:37:07 +0300

    clean up susannes profile page

--- src/components/Susanne.jsx
+++ src/components/Susanne.jsx
@@ -1,27 +1,9 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
-import {
-  CloudArrowUpIcon,
-  LockClosedIcon,
-  ServerIcon,
-} from '@heroicons/react/20/solid';
 
 export function Susanne() {
   return (
     <div className="relative py-24 overflow-hidden bg-white isolate sm:py-32">
-      {/* <div
-        className="absolute -top-80 left-[max(6rem,33%)] -z-10 transform-gpu blur-3xl sm:left-1/2 md:top-20 lg:ml-20 xl:top-3 xl:ml-56"
-        aria-hidden="true"
-      >
-        <div
-          className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
-          style={{
-            clipPath:
-              'polygon(63.1% 29.6%, 100% 17.2%, 76.7% 3.1%, 48.4% 0.1%, 44.6% 4.8%, 54.5% 25.4%, 59.8% 49.1%, 55.3% 57.9%, 44.5% 57.3%, 27.8% 48%, 35.1% 81.6%, 0% 97.8%, 39.3% 100%, 35.3% 81.5%, 97.2% 52.8%, 63.1% 29.6%)',
-          }}
-        />
-      </div> */}
       <div className="px-6 mx-auto max-w-7xl lg:px-8">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <p className="text-lg font-semibold leading-8 tracking-tight text-slate-600">
@@ -69,11 +51,6 @@ export function Susanne() {
                 </p>
               </blockquote>
               <figcaption className="flex mt-8 gap-x-4">
-                {/* <img
-                  src="https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                  className="flex-none w-10 h-10 mt-1 rounded-full bg-gray-50"
-                /> */}
                 <div className="text-sm leading-6">
                   <div className="font-semibold text-gray-900">
                     Hanne Ottmann
@@ -100,48 +77,7 @@ export function Susanne() {
               Susanne er en ettertraktet solist i gravferder og gjør alt for å
               tilfredsstille kundens ønsker.
             </motion.p>
-            {/* <ul role="list" className="max-w-xl mt-8 space-y-8 text-gray-600">
-              <li className="flex gap-x-3">
-                <CloudArrowUpIcon
-                  className="flex-none w-5 h-5 mt-1 text-slate-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    Push to deploy.
-                  </strong>{' '}
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <LockClosedIcon
-                  className="flex-none w-5 h-5 mt-1 text-slate-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    SSL certificates.
-                  </strong>{' '}
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo.
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <ServerIcon
-                  className="flex-none w-5 h-5 mt-1 text-slate-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    Database backups.
-                  </strong>{' '}
-                  Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.
-                  Et magna sit morbi lobortis.
-                </span>
-              </li>
-            </ul> */}
+
             <motion.p
               whileInView={{ opacity: 1 }}
               initial={{ opacity: 0 }}
@@ -156,27 +92,7 @@ export function Susanne() {
               inn og utland. Sammen med sin søster, fiolinisten Kathrine Hvinden
               Hals, har de skapt den populære duoen Søstrene Hals.
             </motion.p>
-            {/* <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-              No server? No problem.
-            </h2> */}
-            {/* <motion.p
-              whileInView={{ opacity: 1 }}
-              initial={{ opacity: 0 }}
-              transition={{
-                delay: 0.5,
-                duration: 1,
-                type: 'fade',
-              }}
-              className="mt-6"
-            >
-              Susanne er høyt ettertraktet som sanger i Norge på grunn av sine
-              faglige sangferdigheter. Hun har samarbeidet med ulike orkestre i
-              fremførelser av oratorier og har opptrådt på nasjonale og
-              internasjonale festivaler og konserter, inkludert Málaga Clásica i
-              Spania. Med sin lidenskap for musikk og en imponerende stemme
-              fortsetter Susanne Hvinden Hals å fortrylle publikum over hele
-              verden.
-            </motion.p> */}
+
             <motion.div
               whileInView={{ opacity: 1 }}
               initial={{ opacity: 0 }}
